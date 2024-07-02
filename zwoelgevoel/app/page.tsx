e8d4f7a0204edcfe30b3794be1b1870a70257463
nvoers <nick-vanoers@hotmail.com>
2024-07-02 12:38:44 +0200

    mobile

--- zwoelgevoel/app/page.tsx
+++ zwoelgevoel/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center w-screen backgroundImage">
-        <h2 className="text-center text-9xl scale-y-125">ZWOEL<br/>GEVOEL</h2>
+    <main className="flex h-screen flex-col items-center justify-center w-screen backgroundImage">
+        <h2 className="text-center md:text-9xl text-7xl scale-y-125">ZWOEL<br/>GEVOEL</h2>
     </main>
   );
 }
