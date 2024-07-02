8ef9e82b09a12b9ebaa18b72e0cce11bd6a73415
nvoers <nick-vanoers@hotmail.com>
2024-07-02 13:59:24 +0200

    animations

---
+++ zwoelgevoel/app/animation/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-around w-screen backgroundImage">
+          <h2 className="text-center md:text-9xl text-7xl scale-y-125 slide">ZWOEL<br/>GEVOEL</h2>
+          <h2 className="text-center md:text-9xl text-7xl scale-y-125 glow">ZWOEL<br/>GEVOEL</h2>
+    </main>
+  );
+}
--- zwoelgevoel/app/page.tsx
+++ zwoelgevoel/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center w-screen backgroundImage">
-        <h2 className="text-center md:text-9xl text-7xl scale-y-125">ZWOEL<br/>GEVOEL</h2>
+        <h2 className="text-center md:text-9xl text-7xl scale-y-125 citylight">ZWOEL<br/>GEVOEL</h2>
     </main>
   );
 }
