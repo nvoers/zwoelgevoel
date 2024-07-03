e515d58354212685d92097132014b3f540407e1e
nvoers <nick-vanoers@hotmail.com>
2024-07-03 11:39:41 +0200

    update redirects

--- zwoelgevoel/next.config.mjs
+++ zwoelgevoel/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/tickets",
+        destination:
+          "https://shop.ikbenaanwezig.nl/tickets/event/zwoel-gevoel-2024",
+        permanent: false,
+        basePath: false,
+      },
+      {
+        source: "/qrcode",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
