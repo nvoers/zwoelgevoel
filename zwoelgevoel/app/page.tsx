31133890e9455c8a1bb4988ec490d8ce4fcde035
nvoers <nick-vanoers@hotmail.com>
2024-08-01 11:43:26 +0200

    remove glow

--- zwoelgevoel/app/page.tsx
+++ zwoelgevoel/app/page.tsx
@@ -7,10 +7,10 @@ const AreaLight = localFont({ src: '../public/AreaNormal-Thin.otf', variable: '-
 export default function Home() {
   return (
       <main className="flex h-screen flex-col items-center justify-center w-screen backgroundImage">
-          <h2 className="text-center md:text-9xl text-7xl scale-y-125 glow">ZWOEL<br/>GEVOEL</h2>
+          <h2 className="text-center md:text-9xl text-7xl scale-y-125">ZWOEL<br/>GEVOEL</h2>
           <h4 className={`pt-16 scale-y-125 ${AreaLight.variable} font-sans`}>DISCO - HOUSE - TECHNO</h4>
           <h4 className={`pt-6 scale-y-125 ${AreaLight.variable} font-sans`}>14 SEPTEMBER</h4>
-          <Link href="/tickets" className={`pt-4 scale-y-125 ${AreaLight.variable} font-sans`}>TICKETS</Link>
+          <Link href="https://shop.ikbenaanwezig.nl/tickets/event/zwoel-gevoel-2024" className={`pt-4 scale-y-125 ${AreaLight.variable} font-sans`}>TICKETS</Link>
       </main>
   );
 }
