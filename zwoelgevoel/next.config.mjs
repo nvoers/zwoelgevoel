7a5727fc3eb9603be5db6522f6518ff792bba389
nvoers <nick-vanoers@hotmail.com>
2024-08-01 12:52:14 +0200

    update ticket link

--- zwoelgevoel/next.config.mjs
+++ zwoelgevoel/next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
         destination: "/linktree",
         permanent: false,
       },
+      {
+        source: "/tickets",
+        destination:
+          "https://shop.ikbenaanwezig.nl/tickets/event/zwoel-gevoel-2024",
+        permanent: false,
+        basePath: false,
+      },
     ];
   },
 };
