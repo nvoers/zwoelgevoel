f08f68be4b69c2b660ae71e8ce782dcd9f100487
Nick van Oers <39125833+nvoers@users.noreply.github.com>
2025-03-10 16:08:33 +0100

    Update next.config.mjs

--- zwoelgevoel/next.config.mjs
+++ zwoelgevoel/next.config.mjs
@@ -12,13 +12,6 @@ const nextConfig = {
         destination: "/linktree",
         permanent: false,
       },
-      {
-        source: "/tickets",
-        destination:
-          "https://shop.ikbenaanwezig.nl/tickets/event/zwoel-gevoel-2024",
-        permanent: false,
-        basePath: false,
-      },
     ];
   },
 };
